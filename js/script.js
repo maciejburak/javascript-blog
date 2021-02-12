cd250df4ca0f4b74dd2de5c0fde05306b99fc2c8
Maciej Burak <maciej.burak97@onet.pl>
2021-02-12 11:50:35 +0100

    Add links in html by js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-'use strict';
+const articles = document.querySelectorAll('.post');
+const titleList = document.querySelector('.titles');
+let html = '';
+
+function generateTitleLinks() {
+    /* for each article */
+    for (let article of articles) {
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+        /* find the title element */
+        const articleTitle = article.querySelector('.post-title').innerHTML;
+        /* create HTML of the link */
+        const linkHTML = '<li><a href=#' + articleId + '><span>' + articleTitle + '</span></a></li>';
+        /* insert link into html variable */
+        html = html + linkHTML;
+        titleList.innerHTML = html;
+    }
+}
+generateTitleLinks();
+
 const titleClickHandler = function (event) {
     const clickedElement = this;
     event.preventDefault();
-    console.log('Link was clicked!');
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
@@ -18,10 +35,8 @@ const titleClickHandler = function (event) {
     }
     /* get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
-    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
-    console.log(targetArticle);
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
 }
@@ -30,3 +45,5 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
+
+
