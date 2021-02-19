b83847b0a06e9b672a9f25f31ee5f9182a78ca2f
Maciej Burak <maciej.burak97@onet.pl>
2021-02-19 12:03:56 +0100

    Add list of tags and authors

--- js/script.js
+++ js/script.js
@@ -43,23 +43,64 @@ for (let link of links) {
 
 //TAGS//
 
-function generateTags() {
+const generateTags = function () {
+    const tagsList = document.querySelector('.tags.list');
+    tagsList.innerHTML = '';
+    const tablica = []
     for (let article of articles) {
         const articleTagsList = article.querySelector('.post-tags .list');
         const articleTags = article.getAttribute('data-tags');
+        //console.log(articleTags);
         const articleTagsArray = articleTags.split(' ');
+        tablica.push(articleTagsArray);
         for (let tag of articleTagsArray) {
-            const linkHTMLWithTag = '<li><a href=#tag-' + tag + '><span>' + tag + '</span></a></li> <br>';
+            const linkHTMLWithTag = '<li><a href=#tag-' + tag + '><span>' + tag + '</span></a></li>';
             articleTagsList.insertAdjacentHTML('beforeend', linkHTMLWithTag);
         }
+    }
+    const arrayWithAllTags = [].concat.apply([], tablica);
+    const finalArray = [];
+    let i = 0
+    for (let element of arrayWithAllTags) {
+        if (finalArray.indexOf(arrayWithAllTags[i]) === -1) {
+            finalArray.push(arrayWithAllTags[i]);
+        }
+        i++;
+    }
+
+    const allTagsHTML = [];
+    const copy = arrayWithAllTags.slice(0);
+    i = 0
+    for (let element of arrayWithAllTags) {
+        let myCount = 0;
+
+        for (var w = 0; w < copy.length; w++) {
+            if (arrayWithAllTags[i] == copy[w]) {
+                myCount++;
+                delete copy[w];
+                w++
+            }
+        }
+        if (myCount > 0) {
+            allTagsHTML.push(myCount);
+        }
+        i++
+    }
+
+    let i1 = 0;
+    for (let elementInFinalArray of finalArray) {
+        const linkHTMLWithTagFromArticle = '<li><a href="#tag-' + finalArray[i1] + '" class= tag-size-' + allTagsHTML[i1] + '>' + finalArray[i1] + '</a> <span></span></li > ';
+        tagsList.insertAdjacentHTML('beforeend', linkHTMLWithTagFromArticle);
+        i1++;
     }
 }
 generateTags();
 
-function tagClickHandler(event) {
+const tagClickHandler = function (event) {
     titlesOfArticles.innerHTML = '';
     const clickedTag = this;
     const tagOfArticle = clickedTag.getAttribute('href').replace('#tag-', '');
+    console.log(tagOfArticle);
     const finalArrayWithTitles = [];
     for (let article of articles) {
         article.classList.remove('active');
@@ -78,12 +119,10 @@ function tagClickHandler(event) {
 
             }
         }
-
     }
 
     const firstArticleWithActive = document.getElementById(finalArrayWithTitles[0]);
     firstArticleWithActive.classList.add('active');
-
     const titleClickHandlerbyLinks = function (event) {
         const clickedElement = this;
         event.preventDefault();
@@ -103,6 +142,14 @@ function tagClickHandler(event) {
     }
 }
 
+function addClickListenersToTagsList() {
+    const linksFromTagList = document.querySelectorAll('.list.tags a')
+    for (let linkFromTagList of linksFromTagList) {
+        linkFromTagList.addEventListener('click', tagClickHandler);
+    }
+}
+addClickListenersToTagsList();
+
 function addClickListenersToTags() {
     const linksFromTags = document.querySelectorAll('.post-tags a')
     for (let linkFromTags of linksFromTags) {
@@ -114,12 +161,56 @@ addClickListenersToTags();
 //AUTHORS//
 
 const addAuthorToArticle = function () {
+    const authorsList = document.querySelector('.authors')
+    const arrayWithAllAuthors = []
     for (let article of articles) {
         const author = article.getAttribute('data-author');
         const authorInHref = author.replace(' ', '-').toLowerCase();
         const titles = article.querySelector('.post-author');
         let linkHTMLWithAuthor = 'by ' + '<a href=#' + authorInHref + '>' + author + '</a>'
         titles.innerHTML = linkHTMLWithAuthor;
+        arrayWithAllAuthors.push(author);
+    }
+    console.log(arrayWithAllAuthors);
+    let i = 0;
+    const finalArrayWithAuthors = [];
+    for (let element of arrayWithAllAuthors) {
+        if (finalArrayWithAuthors.indexOf(arrayWithAllAuthors[i]) === -1) {
+            finalArrayWithAuthors.push(arrayWithAllAuthors[i]);
+        }
+        i++;
+    }
+
+    const authorsLinks = [];
+    let i3 = 0
+    for (let elemenetInfinalArrayWithAuthors of finalArrayWithAuthors) {
+        authorsLinks.push(finalArrayWithAuthors[i3].replace(' ', '-').toLowerCase());
+        i3++;
+    }
+
+    const authorsCount = [];
+    const copy = arrayWithAllAuthors.slice(0);
+    let i1 = 0
+    for (let element1 of arrayWithAllAuthors) {
+        let myCount = 0;
+
+        for (let w = 0; w < copy.length; w++) {
+            if (arrayWithAllAuthors[i1] == copy[w]) {
+                myCount++;
+                delete copy[w];
+            }
+        }
+        if (myCount > 0) {
+            authorsCount.push(myCount);
+        }
+        i1++
+    }
+
+    let i2 = 0
+    for (let elementInFinalArrayWithAuthors of finalArrayWithAuthors) {
+        const linkHTMLWithAuthorsFromArticles = '<li><a href="#' + authorsLinks[i2] + '" class= tag-size-' + authorsCount[i2] + '>' + finalArrayWithAuthors[i2] + '</a> <span></span></li > ';
+        authorsList.insertAdjacentHTML('beforeend', linkHTMLWithAuthorsFromArticles);
+        i2++;
     }
 }
 addAuthorToArticle()
@@ -147,9 +238,9 @@ const addAuthorsToList = function (event) {
             articlesActive.push(arrayWithidFromAuthor[i]);
         }
     }
+
     const targetArticlebylink = document.getElementById(articlesActive[0]);
     targetArticlebylink.classList.add('active');
-
     const titleClickHandlerbyLinks = function (event) {
         event.preventDefault();
         const clickedElement = this;
@@ -174,3 +265,7 @@ for (let authorOfArticles of authorsOfArticles) {
     authorOfArticles.addEventListener('click', addAuthorsToList);
 }
 
+const authorsOfArticlesFromList = document.querySelectorAll('.authors a')
+for (let authorOfArticlesFromList of authorsOfArticlesFromList) {
+    authorOfArticlesFromList.addEventListener('click', addAuthorsToList);
+}
